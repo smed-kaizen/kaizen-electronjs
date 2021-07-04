5d8dce3b2e149ea433d8e853328dcf7be962a96d
mohammed <slimani.med.21@gmail.com>
2021-07-04 18:00:04 +0100

    implementation of the api for the tasks

--- kaizen-api/src/app.ts
+++ kaizen-api/src/app.ts
@@ -17,6 +17,7 @@ import services from './services';
 import appHooks from './app.hooks';
 import channels from './channels';
 import { HookContext as FeathersHookContext } from '@feathersjs/feathers';
+import sequelize from './sequelize';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const app: Application = express(feathers());
@@ -38,6 +39,9 @@ app.use('/', express.static(app.get('public')));
 
 // Set up Plugins and providers
 app.configure(express.rest());
+
+
+app.configure(sequelize);
 
 
 // Configure other middleware (see `middleware/index.ts`)
