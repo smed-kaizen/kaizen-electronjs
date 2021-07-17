ec837c0b6711dc589bf67d1280231d47b6717981
mohammed <slimani.med.21@gmail.com>
2021-07-17 13:17:32 +0200

    New Setup. TS was a mistake, revert to JS

---
+++ electron/main.js
@@ -0,0 +1,54 @@
+
+const { app, BrowserWindow } = require('electron')
+
+const server = require('../server/app')
+
+// console.log('app.get(port)', server.get('port'))
+
+console.log('server', server.services)
+
+function createWindow () {
+  // Create the browser window.
+  console.log('PReload', __dirname + '/preload.js')
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false, // turn off remote
+      preload: __dirname + '/preload.js'
+    }
+  })
+
+  //load the index.html from a url
+  win.loadURL('http://localhost:3000');
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+
