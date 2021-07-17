11596e08db84f0dc17d7e9fdbb5c801a86057c37
mohammed <slimani.med.21@gmail.com>
2021-07-17 13:21:34 +0200

    adding the tasks service

--- server/app.js
+++ server/app.js
@@ -16,6 +16,8 @@ const services = require('./services');
 const appHooks = require('./app.hooks');
 const channels = require('./channels');
 
+const sequelize = require('./sequelize');
+
 const app = express(feathers());
 
 // Load app configuration
@@ -34,6 +36,9 @@ app.use('/', express.static(app.get('public')));
 
 // Set up Plugins and providers
 app.configure(express.rest());
+
+
+app.configure(sequelize);
 
 
 // Configure other middleware (see `middleware/index.js`)
